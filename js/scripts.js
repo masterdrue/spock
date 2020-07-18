509aedb274495257babcb6eeaef44af3f74a61c2
masterdrue <masterdrue@gmail.com>
2020-07-18 12:05:28 -0400

    Final Version

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
-/*coming soon*/
+//Login Modal Button
+
 $(function() {
     $("#loginModalButton").click(function(){
         $('#loginModal').modal('show'); 
     
     });
 });
-/* Toggle Dashboard */
+
+// Transmission Modeling Modal Button
+$(function() {
+    $("#transModalButton").click(function(){
+        $('#transModal').modal('show'); 
+    
+    });
+});
+
+/* Toggle CV-19 Dashboard */
 $(function() {
     $("#btnDashboard").click(function(){
         if ($("#dashboard").collapse("hide").hasClass("dashboard")) {  
@@ -15,57 +25,13 @@ $(function() {
     });
 });
 
-// geting canvas by Boujjou Achraf
-var c = document.getElementById("c");
-var ctx = c.getContext("2d");
+/* Random Last Update Numbers */
 
-//making the canvas full screen
-//c.height = window.innerHeight;
-//c.width = window.innerWidth;
-c.height = 20;
-c.width = 500;
+  document.querySelector('#randomValueMid').textContent = Math.floor(Math.random() * 60) + 1;
+  document.querySelector('#randomValueRight').textContent = Math.floor(Math.random() * 60) + 1;
+  document.querySelector('#randomValueLeft').textContent = Math.floor(Math.random() * 60) + 1;
 
-
-//chinese characters - taken from the unicode charset
-var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
-//converting the string into an array of single characters
-matrix = matrix.split("");
-
-var font_size = 10;
-var columns = c.width/font_size; //number of columns for the rain
-//an array of drops - one per column
-var drops = [];
-//x below is the x coordinate
-//1 = y co-ordinate of the drop(same for every drop initially)
-for(var x = 0; x < columns; x++)
-    drops[x] = 1; 
-
-//drawing the characters
-function draw()
-{
-    //Black BG for the canvas
-    //translucent BG to show trail
-    ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
-    ctx.fillRect(0, 0, c.width, c.height);
-
-    ctx.fillStyle = "#f4427d";//green text
-    ctx.font = font_size + "px arial";
-    //looping over drops
-    for(var i = 0; i < drops.length; i++)
-    {
-        //a random chinese character to print
-        var text = matrix[Math.floor(Math.random()*matrix.length)];
-        //x = i*font_size, y = value of drops[i]*font_size
-        ctx.fillText(text, i*font_size, drops[i]*font_size);
-
-        //sending the drop back to the top randomly after it has crossed the screen
-        //adding a randomness to the reset to make the drops scattered on the Y axis
-        if(drops[i]*font_size > c.height && Math.random() > 0.975)
-            drops[i] = 0;
-
-        //incrementing Y coordinate
-        drops[i]++;
-    }
-}
-
-setInterval(draw, 35);
+//Tool Tip
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
